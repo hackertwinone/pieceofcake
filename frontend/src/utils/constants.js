fb972327a5e2d1ef77e0e284f66799e525f2f8df
hackertwinone <hackertwinone@gmail.com>
2026-06-10 18:48:04 -0500

    api_base constant

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,4 @@
-export const API_BASE = `${process.env.REACT_APP_API_URL || "http://localhost:8000"}/api`;
-
+export const API_BASE = `${import.meta.env.VITE_API_URL || "http://localhost:8000"}/api`;
 export const ROUTES = {
   HOME: "/",
   MENU: "/menu",
