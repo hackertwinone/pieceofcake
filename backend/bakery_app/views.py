fc1c4fb761c0ad4c66c3b6dd7d0bab2b31a4f1b7
hackertwinone <hackertwinone@gmail.com>
2026-06-10 19:45:48 -0500

    serializer update

--- backend/bakery_app/views.py
+++ backend/bakery_app/views.py
@@ -18,7 +18,7 @@ class CategoryListView(generics.ListAPIView):
     serializer_class = CategorySerializer
 
 class MenuItemListView(generics.ListAPIView):
-    serializer_class = 
+    serializer_class = MenuItemSerializer
     
     def get_queryset(self):
         queryset = MenuItem.objects.filter(is_available=True)
