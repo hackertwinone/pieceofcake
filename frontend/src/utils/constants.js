e98a8bd28e6f56de0c1fa6f57526588e1ca657fe
hackertwinone <hackertwinone@gmail.com>
2026-06-10 19:08:09 -0500

    api_base update again

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_URL || "/api";
+export const API_BASE = `${import.meta.env.VITE_API_URL}/api`;
 export const ROUTES = {
   HOME: "/",
   MENU: "/menu",
